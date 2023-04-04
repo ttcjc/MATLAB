498cd772cf4ad67c4c2da029dcba1cdd21139a57
ttcjc <c.j.crickmore@lboro.ac.uk>
2023-04-04 12:23:59 +0100

    Initial Commit of PIV Processing Functions

---
+++ PhD/Functions/initialisePIVdata.m
@@ -0,0 +1,227 @@
+%% PIV Data Initialisation v1.0
+% ----
+% Hi
+% ----
+% Usage: [] = initialisePIVdata();
+%        'Baa' -> Moo
+
+%% Changelog
+
+% v1.0 - Initial Commit
+
+
+%% Main Function
+
+function [caseFolder, caseName, PIVdata, samplingFrequency] = initialisePIVdata(saveLocation, nProc) %#ok<INUSD>
+
+    % Load Previously Collated Data (If Desired/Possible)
+    disp('PIV Data Load');
+    disp('-----------------------------');
+    valid = false;
+    while ~valid
+        disp(' ');
+        selection = input('Load Previously Collated PIV Data? [y/n]: ', 's');
+
+        if selection == 'n' | selection == 'N' %#ok<OR2>
+            break;
+        elseif selection == 'y' | selection == 'Y' %#ok<OR2> 
+            [fileName, filePath] = uigetfile([saveLocation, '/Experimental/MATLAB/*.mat'], ...
+                                             'Select PIV Data');
+
+            if contains(filePath, '/PIV/')
+                disp(['    Loading ''', fileName, '''...']);
+                PIVdata = load([filePath, fileName], 'PIVdata').PIVdata;
+                samplingFrequency = load([filePath, fileName], 'samplingFrequency').samplingFrequency;
+                disp('        Success');
+
+                caseFolder = [];
+                caseName = fileName(1:(end - 4));
+                
+                return;
+            else
+                disp('    WARNING: Invalid File Selection');
+                clear fileName filePath;
+            end
+
+        else
+            disp('    WARNING: Invalid Entry');
+            clear fileName filePath;
+        end
+
+    end
+    clear valid;
+
+    disp(' ');
+    disp(' ');
+
+    % Acquire PIV Data
+    disp('PIV Data Acquisition');
+    disp('------------------------------------');
+    
+    caseFolder = uigetdir('~/Data/Experimental/', 'Select Case');
+    
+    namePos = max(strfind(caseFolder, '/')) + 1;
+    caseName = caseFolder(namePos:end);
+    
+    disp(' ');
+
+    disp(['Case: ', caseName]);
+
+    % Confirm Support
+    if contains(caseFolder, '/Tomographic/')
+        format = 'tomo';
+    else
+        error('Invalid Case Directory (Unsupported Case Type)');
+    end
+    
+    % Confirm Data Availability
+    dataFiles = dir([caseFolder, '/*.dat']);
+    
+    if isempty(dataFiles)
+        error('Invalid Case Directory (No PIV Data Found)');
+    end
+
+    disp(' ');
+
+    disp('***********');
+    disp(' COLLATING ');
+    
+    tic;
+    evalc('parpool(nProc);');
+    
+    disp(' ');
+    
+    disp('    Initialising...');
+    
+    % Specify Sampling Frequency
+    samplingFrequency = str2double(caseName((max(strfind(caseName, '_')) + 1):end));
+    
+    switch format
+
+        case 'planar'
+            % Identify Plane Position
+            % Initialise Position Grid
+            % Convert to Metres and Adjust Origin
+            % Sort Position Grid for Compatibility
+
+        case 'stereo'
+            % Identify Plane Position
+            % Initialise Position Grid
+            % Convert to Metres and Adjust Origin
+            % Sort Position Grid for Compatibility
+
+        case 'tomo'            
+            % Initialise Position Grid
+            fileID = fopen([caseFolder, '/', dataFiles(1).name]);
+            content = textscan(fileID, '%f %f %f %f %f %f %f %f', 'headerLines', 4, 'delimiter', ' ', 'CollectOutput', true);
+            content = content{1};
+            fclose(fileID);
+        
+            % Adjust Co-Ordinate System and Convert to Metres
+            PIVdata.positionGrid = zeros(height(content),3);
+
+            PIVdata.positionGrid(:,1) = -content(:,1) / 1000;
+            PIVdata.positionGrid(:,2) = -content(:,3) / 1000;
+            PIVdata.positionGrid(:,3) = content(:,2) / 1000;
+            
+            % Sort Position Grid for Compatibility 
+            [PIVdata.positionGrid, index] = sortrows(PIVdata.positionGrid,3);
+
+    end
+    
+    disp(' ');
+    
+    % Read Instantaneous Data
+    disp('    Collating Instantaneous PIV Data...');
+    
+    % Initialise Progress Bar
+    wB = waitbar(0, 'Collating Instantaneous PIV Data', 'name', 'Progress');
+    wB.Children.Title.Interpreter = 'none';
+    dQ = parallel.pool.DataQueue;
+    afterEach(dQ, @parforWaitBar);
+
+    parforWaitBar(wB, height(dataFiles));
+    
+    time = zeros(height(dataFiles),1);
+    u = cell(height(dataFiles),1);
+    v = cell(height(dataFiles),1);
+    w = cell(height(dataFiles),1);
+    corrCoeff = cell(height(dataFiles),1);
+    isValid = cell(height(dataFiles),1);
+        
+    switch format
+    
+        case 'planar'
+    
+        case 'stereo'
+    
+        case 'tomo'
+        
+            parfor i = 1:height(dataFiles)
+                fileID = fopen([caseFolder, '/', dataFiles(i).name]);
+                content = textscan(fileID, '%f %f %f %f %f %f %f %f', 'headerLines', 4, 'delimiter', ' ', 'CollectOutput', true);
+                content = content{1};
+                fclose(fileID);
+        
+                time(i) = i * (1 / samplingFrequency);
+                u{i} = content(index,4);
+                v{i} = content(index,5);
+                w{i} = content(index,6);
+                corrCoeff{i} = content(index,7);
+                isValid{i} = content(index,8);
+                
+                send(dQ, []);
+            end
+            
+            delete(wB);
+            
+            PIVdata.time = time;
+            PIVdata.u = u;
+            PIVdata.v = v;
+            PIVdata.w = w;
+            PIVdata.corrCoeff = corrCoeff;
+            PIVdata.isValid = isValid;
+            clear time u v w corrCoeff isValid;
+    
+    end
+
+    evalc('delete(gcp(''nocreate''));');
+    executionTime = toc;
+
+    disp(' ');
+
+    disp(['    Run Time: ', num2str(executionTime), 's']);
+
+    disp(' ');
+
+    disp('  SUCCESS  ');
+    disp('***********');
+    
+%     % Save Data
+%     valid = false;
+%     while ~valid
+%         disp(' ');
+%         selection = input('Save Data for Future Use? [y/n]: ', 's');
+% 
+%         if selection == 'n' | selection == 'N' %#ok<OR2>
+%             valid = true;
+%         elseif selection == 'y' | selection == 'Y' %#ok<OR2>
+%             
+%             if ~exist([saveLocation, '/Experimental/MATLAB/PIV'], 'dir')
+%                 mkdir([saveLocation, '/Experimental/MATLAB/PIV']);
+%             end
+%             
+%             disp(['    Saving to: ', saveLocation, '/Experimental/MATLAB/planarExperimentalSpray/', caseName, '.mat']);
+%             save([saveLocation, '/Experimental/MATLAB/planarExperimentalSpray/', caseName, '.mat'], ...
+%                  'PIVdata', 'samplingFrequency', '-v7.3', '-noCompression');
+%             disp('        Success');
+%             
+%             valid = true;
+%         else
+%             disp('    WARNING: Invalid Entry');
+%         end
+%         
+%     end
+%     clear valid;
+    
+end
